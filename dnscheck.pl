c8120c248b18c0b26b8df92c789532129b6ef0c3
pujiermanto <pujiermanto@gmail.com>
2017-05-25 14:53:02 +0700

    Update dnscheck.pl

--- dnscheck.pl
+++ dnscheck.pl
@@ -2,14 +2,14 @@
 use strict;
 while () {
 my $date = localtime(time());
-        open(CH,"dig \@195.60.0.1 www.bbc.co.uk |") || die "cannot run dig: $!\n";
+        open(CH,"dig \@192.168.1.252 google.com |") || die "cannot run dig: $!\n";
         while (<CH>) {
                 chomp;
                 next unless /query time/i;
                 print $date . "$_\n";
         };
         close CH;
-        open(CH,"dig \@195.60.0.5 www.bbc.co.uk |") || die "cannot run dig: $!\n";
+        open(CH,"dig \@192.168.1.252 google.com |") || die "cannot run dig: $!\n";
         while (<CH>) {
                 chomp;
                 next unless /query time/i;
