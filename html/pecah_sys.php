1ed7a5711f7144d00b6692dced0bf52a2f5b8734
pujiermanto <pujiermanto@gmail.com>
2017-06-10 09:18:21 +0700

    Create pecah_sys.php

---
+++ html/pecah_sys.php
@@ -0,0 +1,22 @@
+<?php
+echo "<br>";
+for($a=1;$a<=100;$a++){
+echo "=";
+}
+
+echo"<br>";
+$z0=$pecah_syslog[0];
+echo "<b>$z0</b>";
+
+echo"<br>";
+$z1=$pecah_syslog[1];
+echo "<b>$z1</b>";
+
+echo"<br>";
+$z2=$pecah_syslog[2];
+echo "<b>$z2</b>";
+
+echo"<br>";
+$z3=$pecah_syslog[3];
+echo "<b>$z3</b>";
+?>
