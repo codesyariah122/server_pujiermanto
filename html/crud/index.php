528669fe8ec863c96219e8a21d35762286eda658
pujiermanto <pujiermanto@gmail.com>
2017-07-17 16:48:24 +0700

    Update index.php

--- html/crud/index.php
+++ html/crud/index.php
@@ -2,7 +2,7 @@
 <?// Puji Ermanto Create A simple crud PHP MYSQL ?>
 <?
 /*-------------------------------------------
-					Kontak
+		Kontak
 ---------------------------------------------
 Emali : pujiermanto@gmail.com,
 phone : 0895345041053	
