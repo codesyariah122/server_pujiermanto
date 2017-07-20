a536c9a9c676bf960cceaf29a24352ef94404da8
pujiermanto <pujiermanto@gmail.com>
2017-07-20 18:55:00 +0700

    Update waktos.php

--- html/waktos.php
+++ html/waktos.php
@@ -9,25 +9,21 @@
 </head>
 <h1 id="waktos"></h1>
 <body>
-						<script type="text/javascript">
-						window.onload = function() { waktos(); }
-
-							function waktos() {
-								var e = document.getElementById('waktos'),
-									d = new Date(), h, m, s;
-									h = d.getHours();
-									m = set(d.getMinutes());
-									s = set(d.getSeconds());
-
-									e.innerHTML = h +':'+ m +':'+ s;
-
-								setTimeout('waktos()', 1000);
-							}
-
-								function set(e) {
-								e = e < 10 ? '0'+ e : e;
-										return e;
-									}
-						</script>
+<script type="text/javascript">
+	window.onload = function() { waktos(); }
+	function waktos() {
+	var e = document.getElementById('waktos'),
+	    d = new Date(), h, m, s;
+	    h = d.getHours();
+	    m = set(d.getMinutes());
+	    s = set(d.getSeconds());
+	e.innerHTML = h +':'+ m +':'+ s;
+	setTimeout('waktos()', 1000);
+	}
+	function set(e) {
+		 e = e < 10 ? '0'+ e : e;
+		 return e;
+}
+</script>
 </body>
 </html>
