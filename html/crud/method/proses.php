acf1657fd9460f58f8efeea61f12bb3b4798a8a6
pujiermanto <pujiermanto@gmail.com>
2017-07-29 13:39:10 +0700

    Update proses.php

--- html/crud/method/proses.php
+++ html/crud/method/proses.php
@@ -7,7 +7,7 @@ $tgl=date('y/m/d H:i:s');
 $nama=$_REQUEST['nama'];
 $email=$_REQUEST['email'];
 $telp=$_REQUEST['telp'];
-$alamat=nl2br($_REQUEST['alamat']);
+$alamat=strip_tags (nl2br($_REQUEST['alamat']));
 if((!empty($nama))&&(!empty($email))&&(!empty($telp))&&(!empty($alamat))){
 $query_data=mysql_query("insert into data values(null,'$tgl','$nama','$email','$telp','$alamat')");
 if(!$query_data):
